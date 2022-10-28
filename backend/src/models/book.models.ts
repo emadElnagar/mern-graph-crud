2badad5fdac10a0ba5f51f858991d0974ed06215
unknown <emad.m.alnagar@gmail.com>
2022-10-28 21:49:35 +0200

    Book model

---
+++ backend/src/models/book.models.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose';
+
+interface Book {
+  title: string,
+  brief: string,
+  author: string
+}
+
+const bookSchema = new Schema<Book>({
+  title: { type: String, required: true },
+  brief: { type: String, required: true },
+  author: { type: String, required: true }
+}, {
+  timestamps: true
+});
+
+const Book = model("Book", bookSchema);
+export default Book;
