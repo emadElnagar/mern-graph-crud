290e0a2cbd5e70907a2a53c398876141e6557256
unknown <emad.m.alnagar@gmail.com>
2022-10-29 17:28:34 +0200

    Get all books query

--- backend/src/graphql/query/book.query.ts
+++ backend/src/graphql/query/book.query.ts
@@ -1,12 +1,16 @@
 import { GraphQLList, GraphQLObjectType } from 'graphql';
 import bookType from '../types/book.types';
+import Book from '../../models/book.models';
 
 const GET_ALL_BOOKS = new GraphQLObjectType({
   name: 'Query',
   description: 'This is the query type',
   fields: {
     books: {
-      type: new GraphQLList(bookType)
+      type: new GraphQLList(bookType),
+      resolve() {
+        return Book.find();
+      }
     }
   }
 });
