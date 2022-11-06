78491b6cd3243078ab40c97e1c7f929570c686dd
unknown <emad.m.alnagar@gmail.com>
2022-11-06 21:16:15 +0200

    Update book mutation

--- backend/src/graphql/mutations/books.mutations.ts
+++ backend/src/graphql/mutations/books.mutations.ts
@@ -2,6 +2,7 @@ import { GraphQLString } from 'graphql';
 import Book from '../../models/book.models';
 import bookType from '../types/book.types';
 
+// CREATE NEW BOOK
 export const CREATE_NEW_BOOK = {
   type: bookType,
   args: {
@@ -17,5 +18,23 @@ export const CREATE_NEW_BOOK = {
     }
     const newBook = await new Book({ title, brief, author }).save();
     return newBook;
+  },
+}
+
+// UPDATE BOOK
+export const UPDATE_BOOK = {
+  args: {
+    title: { type: GraphQLString },
+    brief: { type: GraphQLString },
+    author: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { title, brief, author } = args;
+    const book = await Book.findOne({title: title});
+    if (!book) {
+      throw new Error("This book doesn't exist");
+    }
+    await Book.updateOne({ title: title }, { brief: brief }, { author: author });
+    return { successful: true, message: "Book updated successfully" };
   },
 }
