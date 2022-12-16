02083eb76803e3462373884cf4777c7d1651daf0
unknown <emad.m.alnagar@gmail.com>
2022-12-16 20:45:40 +0200

    Error message

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,6 +1,10 @@
+import { useBooks } from "../hooks/books";
+
 function ErrorBox() {
+  const { error } = useBooks();
+  const message = error?.message;  
   return (
-    <div className="error">error</div>
+    <div className="error">{ message }</div>
   )
 }
 
