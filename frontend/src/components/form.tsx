f6a65e9a8db779ce6fa90718773649009b235485
unknown <emad.m.alnagar@gmail.com>
2023-01-02 16:21:15 +0200

    Show craete new book error

--- frontend/src/components/form.tsx
+++ frontend/src/components/form.tsx
@@ -1,18 +1,25 @@
 import { useMutation } from "@apollo/client";
 import { useState } from "react";
+import Swal from "sweetalert2";
 import { CREATE_NEW_BOOK } from "../hooks/NewBook";
 
 function Form() {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [brief, setBrief] = useState('');
-  const [createNewBook, {error}] = useMutation(CREATE_NEW_BOOK);
+  const [createNewBook, { error }] = useMutation(CREATE_NEW_BOOK);
   const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
+    if(error) {
+      Swal.fire({
+        title: 'Error',
+        text: `${ error.message }`,
+        icon: 'warning',
+      });
+    }
     createNewBook({
       variables: { title, author, brief }
     });
-    console.log(error);    
   }
   return (
     <div className="paper">
