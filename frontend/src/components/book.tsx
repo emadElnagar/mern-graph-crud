0e1e2c8dd1da3557eb888376a8cea1908a5cdff5
unknown <emad.m.alnagar@gmail.com>
2023-01-06 14:17:18 +0200

    Show and hide updating form

--- frontend/src/components/book.tsx
+++ frontend/src/components/book.tsx
@@ -1,4 +1,4 @@
-import { Key } from "react";
+import { Fragment, Key, useState } from "react";
 import { useMutation } from "@apollo/client";
 import { DELETE_BOOK } from "../hooks/deleteBook";
 import LoadingBox from "./LoadingBox";
@@ -16,42 +16,59 @@ interface BookProps {
 }
 
 const Book = ({ _id, title, author }: BookProps) => {
+  const [isUpdating, setIsUpdating] = useState(false);
   const [deleteBook, { error, loading }] = useMutation(DELETE_BOOK);
   return (
     loading ? <LoadingBox /> :
     error ? <ErrorBox /> :
-    <li className="book">
-      <span className="book-name">
-        <Link to={`${_id}`}>{title}</Link>
-      </span>
-      <div className="buttons">
-        <button className="update"><HiPencil /></button>
-        <button className="delete" onClick={() => {
-          Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              deleteBook({
-                variables: { id: _id },
-                refetchQueries: [{ query: GET_BOOKS }]
-              });
-              Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-              )
-            }
-          });
-        }}><AiFillDelete /></button>
+    <Fragment>
+      <li className={`book ${isUpdating === true ? 'd-none' : ''}`}>
+        <span className="book-name">
+          <Link to={`${_id}`}>{title}</Link>
+        </span>
+        <div className="buttons">
+          <button className="update" onClick={() => setIsUpdating(true)}><HiPencil /></button>
+          <button className="delete" onClick={() => {
+            Swal.fire({
+              title: 'Are you sure?',
+              text: "You won't be able to revert this!",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                deleteBook({
+                  variables: { id: _id },
+                  refetchQueries: [{ query: GET_BOOKS }]
+                });
+                Swal.fire(
+                  'Deleted!',
+                  'Your file has been deleted.',
+                  'success'
+                )
+              }
+            });
+          }}><AiFillDelete /></button>
+        </div>
+        <span className="author"><small>by: {author}</small></span>
+      </li>
+      {/* UPDATE FORM */}
+      <div className={`update-from ${isUpdating === true ? 'show' : ''}`}>
+        <form>
+          <div className="fields">
+            <input defaultValue={title} type="text" placeholder="Book name" />
+            <input defaultValue={author} type="text" placeholder="Book autor" />
+          </div>
+          <textarea placeholder="Brief about the book"></textarea>
+          <div className="buttons">
+            <input type="submit" value="submit" />
+            <button className="cancel" onClick={() => setIsUpdating(false)}>cancel</button>
+          </div>
+        </form>
       </div>
-      <span className="author"><small>by: {author}</small></span>
-    </li>
+    </Fragment>
   )
 }
 
